e3114da29198202f2e630428c95608528b970fc9
Justin Cummings <justin.lewis.cummings@gmail.com>
2021-12-04 13:12:21 -0600

    started on reducers

--- src/index.js
+++ src/index.js
@@ -9,9 +9,43 @@ import logger from 'redux-logger';
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import {Provider} from 'react-redux';
 
-const feelingsReducer () => {
-    
+const feelingsReducer =  (state = [], action => {
+
 }
+return state;
+
+const understandReducer = (state = [], action) => {
+
+}
+return state;
+
+const supportReducer = (state = [], action) => {
+
+}
+return state;
+
+const commentsReducer = (state = [], action ) => {
+
+}
+return state;
+
+const storeInstance = createStore{
+    combineReducers({
+        supportReducer,
+        feelingsReducer,
+        understandReducer
+    }),
+    applyMiddleware
+};
+
+ReactDOM.render(
+    <Provider store={storeInstance}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+)
+
+
 
 
 
