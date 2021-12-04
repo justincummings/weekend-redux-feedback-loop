1f481e1deae4378bc23cd9777227f3685ffe1da3
Justin Cummings <justin.lewis.cummings@gmail.com>
2021-12-04 11:27:58 -0600

    created components

---
+++ src/components/feelings/feelings.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import import { } from 'react-redux'
+
+
+function feelings() {
+
+
+    
+    return (
+        <div>
+            How are you feeling today?
+            <br> <br/>
+            <input type="text"> 
+        </div>
+    )
+}
+
+export default feelings
