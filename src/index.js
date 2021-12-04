c84cb775dbccea2c050202fd06bc9fc477e31465
Justin Cummings <justin.lewis.cummings@gmail.com>
2021-12-04 11:35:06 -0600

    imported redux

--- src/index.js
+++ src/index.js
@@ -4,5 +4,18 @@ import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
 
+//importing redux logger, store, reducers & middleware
+import logger from 'redux-logger';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import {Provider} from 'react-redux';
+
+const feelingsReducer () => {
+    
+}
+
+
+
+
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
